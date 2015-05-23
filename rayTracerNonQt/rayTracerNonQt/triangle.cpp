fc449d4a96aae12e13eaf87ad1fde015e051316b
Ruan Leitão Nunes <ruanrln@gmail.com>
2015-05-23 15:26:10 -0300

    arrumando algoritmo de hit test do triangulo

--- rayTracerNonQt/rayTracerNonQt/triangle.cpp
+++ rayTracerNonQt/rayTracerNonQt/triangle.cpp
@@ -1,6 +1,8 @@
 #include "triangle.h"
 #include <cmath>
 
+#include <iostream>
+
 Triangle::Triangle()
 {
 
@@ -8,32 +10,39 @@ Triangle::Triangle()
 
 Intersection Triangle::hitTest(Ray ray, bool *success)
 {
+    Intersection intersec;
     Vector3D edge1, edge2, edge3, triangleNormal;
-    double epsilon = 0.00001;
-    double dir, d, t;
+    double epsilon = 0.0001;
+    double dir, t;
     *success = true;
 
     edge1 = this->v2 - this->v1;
     edge2 = this->v3 - this->v1;
     triangleNormal = edge1.crossProduct(edge2);
-    triangleNormal.normalize();
+    triangleNormal;
+    Vector3D direction = ray.getDirection();
+    //using v1 as a point of the plane
+    double den = triangleNormal.getX() * direction.getX() + triangleNormal.getY() * direction.getY() + triangleNormal.getZ() * direction.getZ();
+    double d = -(triangleNormal.getX() * this->v1.getX() + triangleNormal.getY() * this->v1.getY() + triangleNormal.getZ() * this->v1.getZ());
 
-    dir = ray.getDirection().dotProduct(triangleNormal);
-    if(fabs(dir) < epsilon) {
+    //dir = ray.getDirection().dotProduct(triangleNormal);
+    if(fabs(den) < epsilon) {
         *success = false;
+        return intersec;
     }
-
-    d = triangleNormal.dotProduct((this->v1).toVector());
-    t = triangleNormal.dotProduct(ray.getOrigin().toVector());
-    t += d;
-    t /= dir;
+    Point3D orig = ray.getOrigin();
+    double num = triangleNormal.getX() * orig.getX() + triangleNormal.getY() * orig.getY() + triangleNormal.getZ() * orig.getZ() + d;
+    t = -(num/den);
+   // std::cout << "intersecta o plano anyway, t: " << t << std::endl;
     if (t < 0) {
         *success = false;
+        return intersec;
     }
-
-    Vector3D normal;
     Point3D intersectionPoint;
-    intersectionPoint = ray.getOrigin() + ray.getDirection().scalarProduct(t);
+    Vector3D rayDirection = ray.getDirection();
+    rayDirection.normalize();
+    intersectionPoint = ray.getOrigin() + rayDirection.scalarProduct(t);
+    //std::cout << "intersectou o plano em positivo: " << intersectionPoint.toString() << std::endl;
 
     Vector3D perpendicular;
     edge1 = this->v2 - this->v1;
@@ -55,7 +64,8 @@ Intersection Triangle::hitTest(Ray ray, bool *success)
         *success = false;
     }
 
-    Intersection intersec;
+    triangleNormal.normalize();
+    //triangleNormal = triangleNormal.scalarProduct(-1);
     intersec.setNormalVector(triangleNormal);
     intersec.setIntersectionPoint(intersectionPoint);
     intersec.setT(t);
