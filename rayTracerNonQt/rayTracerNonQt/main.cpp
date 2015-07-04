4e1a260f2c13bd368b279ba5cf15e6707a52aa08
Ruan Leitão Nunes <ruanrln@gmail.com>
2015-07-04 01:18:57 -0300

    adicionando um timer simples para testes iniciais, mede em segundos

--- rayTracerNonQt/rayTracerNonQt/main.cpp
+++ rayTracerNonQt/rayTracerNonQt/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 #include "scene.h"
 #include "vector3d.h"
 
@@ -7,10 +8,16 @@ using namespace std;
 int main()
 {
     Scene scene;
+    clock_t t1,t2;
+    t1=clock();
     scene.readScene("scene01-zoom-ss-reflect-lights-shadows.yaml");
-    std::cout << "time 1, cena lida" << std::endl;
+    //code goes here
     scene.renderScene("face.png");
     std::cout << "time 2, cena renderizada" << std::endl;
+    t2=clock();
+    float diff = ((float)t2-(float)t1) / CLOCKS_PER_SEC;
+
+    std::cout<<diff<<std::endl;
     return 0;
 }
 
